b69fd908d3565ab3e99563ce76547396c003e3d1
Payden <69829508+Payden1224@users.noreply.github.com>
2023-05-30 16:44:05 -0500

    func main

---
+++ quiz.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to my quiz game!")
+
+	fmt.Printf("Enter your name: ")
+	var name string
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello, %v, welcome to the game!\n", name)
+
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+
+	if age >= 10 {
+		fmt.Println("Yay you can play!")
+	} else {
+		fmt.Println("You cannot play!")
+		return
+	}
+
+	fmt.Printf("What is better, the RTX 3080 or RTX 3090? ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer, &answer2)
+
+	if answer +  " " + answer2 == "RTX 3090" {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Incorrect!")
+	}
+}
